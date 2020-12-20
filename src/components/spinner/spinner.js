70a9eee116c72b551080fa41811ae9191c83b29b
Fle1ta <grey5boyko@yndex.ru>
2020-12-21 01:53:36 +0300

    added some adaptive styles

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -3,7 +3,7 @@ import './spinner.css';
 
 const Spinner = () => {
     return (<div className='spinner__container'>  
-                <div class="loadingio-spinner-spinner-hv9vq4k6slj"><div class="ldio-zcrwtigbkw">
+                <div className="loadingio-spinner-spinner-hv9vq4k6slj"><div className="ldio-zcrwtigbkw">
                 <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
                 </div></div>
             </div>)
